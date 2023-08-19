d1b3c022a611306f502a3d17490acdbdec3c8fd6
afect13 <ns130192@gmail.com>
2023-08-19 11:53:54 +0500

    feat: added bage workflow to README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # aston-react-project
+
+![workflow](https://github.com/afect13/aston-react-project/actions/workflows/github-actions.yml/badge.svg)
--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,7 +1,9 @@
 export const Main = () => {
   return (
     <main className="w-full min-h-screen font-poppins">
-      <section className="max-w-screen-lg mx-auto"></section>
+      <section className="max-w-screen-lg mx-auto">
+        <p>Hello World</p>
+      </section>
     </main>
   );
 };
