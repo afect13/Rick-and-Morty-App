e2454d686815ea3f88b941d6d8668e250d7cd608
afect13 <ns130192@gmail.com>
2023-08-20 11:36:14 +0500

    feat: added page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,7 @@
 export * from './main/Main';
+export * from './favorites/Favorites';
+export * from './history/History';
+export * from './search/Search';
+export * from './signin/Signin';
+export * from './signup/Signup';
+export * from './notFound/NotFound';
--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,21 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import App from './App';
-import { Main } from './pages';
+import { Favorites, History, Main, NotFound, Search, Signin, Signup } from './pages';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    children: [{ path: `/`, element: <Main /> }],
+    children: [
+      { path: '', element: <Main /> },
+      { path: '/favorites', element: <Favorites /> },
+      { path: '/history', element: <History /> },
+      { path: '/search', element: <Search /> },
+      { path: '/signin', element: <Signin /> },
+      { path: '/signup', element: <Signup /> },
+      { path: '*', element: <NotFound /> },
+    ],
   },
 ]);
 export default router;
