f3a671c0dcc75ce407b1060c883c0abb826937b5
afect13 <ns130192@gmail.com>
2023-08-22 11:22:14 +0500

    feat: added dark and ligth theam

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
@@ -9,6 +10,7 @@ function App() {
   const toggleTheam = () => {
     setIsDark(!isDark);
   };
+
   return (
     <ThemeContext.Provider
       value={{
@@ -16,12 +18,22 @@ function App() {
         toggleTheam,
       }}
     >
-      <header className="w-full font-poppins bg-zinc-800 ">
+      <header
+        className={classNames('w-full font-poppins', {
+          ['bg-zinc-800']: isDark,
+          ['bg-zinc-200']: !isDark,
+        })}
+      >
         <nav className="max-w-screen-lg py-4 mx-auto">
           <Menu />
         </nav>
       </header>
-      <main className="w-full min-h-screen font-poppins bg-zinc-700">
+      <main
+        className={classNames('w-full min-h-screen font-poppins', {
+          ['bg-zinc-700']: isDark,
+          ['bg-zinc-400']: !isDark,
+        })}
+      >
         <section className="max-w-screen-lg mx-auto">
           <Outlet />
         </section>
