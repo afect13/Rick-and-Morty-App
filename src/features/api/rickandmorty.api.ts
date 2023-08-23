8d51a345b1abfc854df5b8884f9cd530c537f942
afect13 <ns130192@gmail.com>
2023-08-23 11:11:51 +0500

    feat: init createApi RTK added rickandmortyApi

---
+++ src/features/api/rickandmorty.api.ts
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+import { Character, ServerResponse } from '../../models/characters';
+
+export const rickandmortyApi = createApi({
+  reducerPath: 'rickandmorty/api',
+
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://rickandmortyapi.com/api/' }),
+
+  endpoints: (builder) => ({
+    getCharacter: builder.query<Character[], void>({
+      query: () => 'character',
+      transformResponse: (response: ServerResponse<Character>) => response.results,
+    }),
+  }),
+});
+
+export const { useGetCharacterQuery } = rickandmortyApi;
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import { rickandmortyApi } from '../features';
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [rickandmortyApi.reducerPath]: rickandmortyApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(rickandmortyApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
