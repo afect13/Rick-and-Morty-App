516d2efcf52a326a235d9642d957181b3d3cefba
afect13 <ns130192@gmail.com>
2023-09-07 09:04:50 +0500

    fix: favorite btn in card

---
+++ src/hooks/toggleFavorite/toggleFavorite.ts
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+
+import { getFavorites, toggleFavorites } from '../../features';
+import { useAppDispatch } from '../../store';
+
+type ToggleFavorites = {
+  isLoading: boolean;
+  isFavorite: boolean;
+  handleToggleFavorite: () => void;
+};
+export const useToggleFavorite = (id: number): ToggleFavorites => {
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useAppDispatch();
+  const isFavorite = useSelector(getFavorites).includes(id);
+
+  const handleToggleFavorite = async () => {
+    setIsLoading(true);
+    await dispatch(toggleFavorites(id));
+    setIsLoading(false);
+  };
+  return { isLoading, isFavorite, handleToggleFavorite };
+};
